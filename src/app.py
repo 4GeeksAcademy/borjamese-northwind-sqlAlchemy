508a2f19e247690b8077718860d45039f0f7abc7
Borjamese <119245691+Borjamese@users.noreply.github.com>
2023-05-15 18:57:46 +0000

    a medias

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Product, Supplier, Order
 #from models import Person
 
 app = Flask(__name__)
@@ -45,7 +45,57 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+def create_product ():
+        product =Product(product_name="Mandarina", list_price = 0.2)
+        db.session.add(product)
+        db.session.commit()
+
+def get_product(id):
+    product = Product.query.get(1)
+    print(product.product_name)
+
+def create_order(city):
+    order = Order(ship_city = city)
+    db.session.add(order)
+    db.session.commit()
+
+
+def get_all_products():
+    products = Product.query.all()
+    for p in products:
+        print(p)
+
+def get_cheap_products():
+    products = Product.query.filter(Product.list_price < 1).all ()
+    for p in products:
+        print(p)
+
+
+def get_all_products_of_a_single_supplier(id):
+    supplier= Supplier.query.get(id)
+    print(f'Productos para la compañia: {supplier.company_name}')
+    for p in supplier.products:
+        print(p)
+
+def insert_suppliers():
+    cocacola = Supplier(company_name='Coca Cola', homepage='https://cocacola.com')
+    db.session.add(cocacola)
+    fruitco = Supplier(company_name='Fruits Company', homepage='https://piñacolada.com')
+    db.session.add(fruitco)
+    db.session.commit()
+
+
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+   #app.run(host='0.0.0.0', port=PORT, debug=False)
+    with app.app_context():
+       # create_product()
+       # get_product(1)
+       # get_all_products()
+       # get_cheap_products()
+       insert_suppliers()
+       #get_all_products_of_a_single_supplier(1)
+       #create_order("Barcelona")
+       
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -17,3 +18,30 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(120), nullable=False)
+    list_price = db.Column(db.Float, nullable=False)
+
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=True)
+
+    supplier = relationship('Supplier', backref='products')
+
+    def __repr__(self):
+        return f'<Product {self.product_name} - {self.list_price}> $ y pertenece al proveedor {self.supplier.company_name}>'
+
+
+
+class Supplier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_name =  db.Column(db.String(120),unique=True, nullable=False)
+    # Aún hay empresas sin página Web...
+    homepage = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'<Supplier {self.company_name}'
+    
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ship_city = db.Column(db.String(80), unique=False, nullable=False)
